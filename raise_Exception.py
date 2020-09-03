ef603fedea3a9d38f1f3cd6c6aa175217a56cdc1
pankaj pandey <49112512+pankz-104@users.noreply.github.com>
2020-09-03 13:28:04 +0530

    Create raise_Exception.py

---
+++ raise_Exception.py
@@ -0,0 +1,20 @@
+# raise
+
+# a = input("what is your name \n")
+# b = input("how much do you earn \n")
+# if int(b) == 0:
+#     raise ZeroDivisionError("b is 0 so stoppin program")
+# if a.isnumeric():
+#     raise Exception("numbers are not allowed")
+#
+# print(f"Hello {a}")
+
+# c = input("Enter your name")
+# try:
+#     print(a)
+#
+# except Exception as e:
+#     if c == "Pankaj":
+#         raise ValueError("Pankaj is fired from company")
+#
+#     print("Excepition handled")
